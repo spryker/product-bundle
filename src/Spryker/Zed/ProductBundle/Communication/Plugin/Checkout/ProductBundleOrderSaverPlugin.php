c9e78c16c0fadaa19a7786bbc3107eebcf2e992e
Denis Turkov <denis.turkov@spryker.com>
2017-11-02 16:37:21 +0000

    CORE-1296: Refactor doSave plugins to use a SaveOrderTransfer directly and do not change the checkout response

--- src/Spryker/Zed/ProductBundle/Communication/Plugin/Checkout/ProductBundleOrderSaverPlugin.php
+++ src/Spryker/Zed/ProductBundle/Communication/Plugin/Checkout/ProductBundleOrderSaverPlugin.php
@@ -5,17 +5,15 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\ProductBundle\Communication\Plugin\Sales;
+namespace Spryker\Zed\ProductBundle\Communication\Plugin\Checkout;
 
 use Generated\Shared\Transfer\CheckoutResponseTransfer;
 use Generated\Shared\Transfer\QuoteTransfer;
-use Spryker\Zed\Checkout\Dependency\Plugin\CheckoutSaveOrderInterface;
+use Generated\Shared\Transfer\SaveOrderTransfer;
+use Spryker\Zed\Checkout\Dependency\Plugin\PlaceOrder\CheckoutSaveOrderInterface;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 
 /**
- * @deprecated Use \Spryker\Zed\ProductBundle\Communication\Plugin\Checkout\ProductBundleOrderSaverPlugin instead
- * Will be removed with the next major release
- *
  * @method \Spryker\Zed\ProductBundle\Business\ProductBundleFacade getFacade()
  * @method \Spryker\Zed\ProductBundle\Communication\ProductBundleCommunicationFactory getFactory()
  */
@@ -25,12 +23,12 @@ class ProductBundleOrderSaverPlugin extends AbstractPlugin implements CheckoutSa
      * @api
      *
      * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
-     * @param \Generated\Shared\Transfer\CheckoutResponseTransfer $checkoutResponse
+     * @param SaveOrderTransfer $saveOrderTransfer
      *
      * @return void
      */
-    public function saveOrder(QuoteTransfer $quoteTransfer, CheckoutResponseTransfer $checkoutResponse)
+    public function saveOrder(QuoteTransfer $quoteTransfer, SaveOrderTransfer $saveOrderTransfer)
     {
-         $this->getFacade()->saveSalesOrderBundleItems($quoteTransfer, $checkoutResponse);
+         $this->getFacade()->saveOrderBundleItems($quoteTransfer, $saveOrderTransfer);
     }
 }
